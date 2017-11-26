b8d847ee498ee7668fa212b755510e272b90b07f
Gilles Duboscq <gilles.m.duboscq@gmail.com>
2017-11-26 23:59:04 +0100

    Add simple command line parsin

--- src/gd/twohundred/jvb/Main.java
+++ src/gd/twohundred/jvb/Main.java
@@ -1,22 +1,32 @@
 package gd.twohundred.jvb;
 
-import gd.twohundred.jvb.components.cpu.CPU;
 import gd.twohundred.jvb.components.CartridgeRAM;
 import gd.twohundred.jvb.components.CartridgeROM;
 import gd.twohundred.jvb.components.Debugger;
 import gd.twohundred.jvb.components.VirtualBoy;
+import gd.twohundred.jvb.components.cpu.CPU;
+import picocli.CommandLine;
 
 import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.concurrent.locks.LockSupport;
 
 import static gd.twohundred.jvb.Logger.Component.Misc;
 import static java.lang.Long.min;
 
 public class Main {
+    @CommandLine.Parameters(description = "ROM file to execute", paramLabel = "ROM")
     private Path cartridgePath;
+
+    @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "display a help message")
+    private boolean helpRequested = false;
+
+    @CommandLine.Option(names = { "-d", "--debugger" }, description = "enable text-mode debugger")
+    private boolean useDebugger = false;
+
+    @CommandLine.Option(names = { "-p", "--paused" }, description = "Start the debugger in paused mode")
+    private boolean pauseOnStart = false;
 
     private static final long NS_PER_CYCLES = Utils.NANOS_PER_SECOND / CPU.CLOCK_HZ;
     private static final long TARGET_MACRO_TICK_PER_SECOND = 100;
@@ -25,12 +35,35 @@ public class Main {
 
     public static void main(String... args) throws IOException {
         Main m = new Main();
-        m.cartridgePath = Paths.get(args[0]);
+        try {
+            CommandLine commandLine = new CommandLine(m);
+            commandLine.registerConverter(java.nio.file.Path.class, s -> java.nio.file.Paths.get(s));
+            commandLine.parse(args);
+            m.checkParameters(commandLine);
+            if (commandLine.isUsageHelpRequested()) {
+                commandLine.usage(System.out);
+                return;
+            } else if (commandLine.isVersionHelpRequested()) {
+                commandLine.printVersionHelp(System.out);
+                return;
+            }
+        } catch (CommandLine.ParameterException ex) {
+            System.err.println(ex.getMessage());
+            ex.getCommandLine().usage(System.err);
+            System.exit(2);
+        }
+
         try {
             m.run();
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
+        }
+    }
+
+    private void checkParameters(CommandLine commandLine) {
+        if (pauseOnStart && !useDebugger) {
+            throw new CommandLine.ParameterException(commandLine, "--paused can not be used without --debugger");
         }
     }
 
@@ -48,17 +81,16 @@ public class Main {
             DefaultAudioOut audioOut = new DefaultAudioOut(logger);
             VirtualBoy virtualBoy = new VirtualBoy(mainWindow.getScreen(), audioOut, inputProvider, rom, new CartridgeRAM(), logger);
             virtualBoy.reset();
-            try {
+            if (useDebugger) {
                 debugger = new Debugger();
-                //debugger.pause();
                 logger.setDestination(debugger);
-            } catch (RuntimeException re) {
-                logger.setDestination(new StdLogger());
-                logger.error(Misc, re, "Could not create debugger");
-            }
-            if (debugger != null) {
+                if (pauseOnStart) {
+                    debugger.pause();
+                }
                 debugger.attach(virtualBoy);
                 debugger.refresh();
+            } else {
+                logger.setDestination(new StdLogger());
             }
             long startT = System.nanoTime();
             long cycles = 0;
@@ -75,7 +107,7 @@ public class Main {
                     //System.out.println(missingCycles + " " +  missingTime / NS_PER_CYCLES);
                     long cyclesDone = virtualBoy.tick(missingCycles);
                     cycles += cyclesDone;
-                } else if (-missingTime > 10000){
+                } else if (-missingTime > 10000) {
                     LockSupport.parkNanos(-missingTime);
                 }
             }
